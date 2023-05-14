f5fdc69fc4fb9613400803c48b5d4e004da1181a
ryan <ryanqueiroz2012@gmail.com>
2023-05-14 18:43:05 -0300

    Subindo Projeto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import Memory from './images/icon-memory.svg'
+import Reaction from './images/icon-reaction.svg'
+import Verbal from './images/icon-verbal.svg'
+import Visual from './images/icon-visual.svg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="result">
+        <h3>Your Result</h3>
+        <div className="seventysix">
+          <h1>76</h1>
+          <p>of 100</p>
+        </div>
+        <h2>Great</h2>
+        <h6>You scored higher than 65% of the people who have taken these tests.</h6>
+      </div>
+      <div className="summary">
+        <h3>Summary</h3>
+        <div className="summaryResults">
+          <div className="group1">
+            <img src={Reaction} alt="#" />
+            <h5 id="Reaction">Reaction</h5>
+            <h4 id="results">80 /</h4>
+            <h4>100</h4>
+          </div>
+          <div className="group2">
+            <img src={Memory} alt="#" />
+            <h5 id="Memory">Memory</h5>
+            <h4 id="results">91 /</h4>
+            <h4>100</h4>
+          </div>
+          <div className="group3">
+            <img src={Verbal} alt="#" />
+            <h5 id="Verbal">Verbal</h5>
+            <h4 id="results2">61 /</h4>
+            <h4>100</h4>
+          </div>
+          <div className="group4">
+            <img src={Visual} alt="#" />
+            <h5 id="Visual">Visual</h5>
+            <h4 id="results2">72 /</h4>
+            <h4>100</h4>
+          </div>
+        </div>
+        <button>Continue</button>
+      </div>
     </div>
   );
 }
